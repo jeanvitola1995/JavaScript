1bf970a7ceec2770ad2c5045bc6ac2d8a7b0b5fd
jeanvitola1995 <jvitola@utb.edu.co>
2022-09-30 16:27:23 +0000

    ejercicio String

---
+++ ejercicio/string.js
@@ -0,0 +1,34 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+
+
+// name and last name
+let name = "Jean Carlos"
+let last_name = "vitola cabarcas"
+
+
+// string studiante concat
+let estudiante = `${name} ${last_name}`
+
+//estudianteMayus
+let estudianteMayus = (estudiante).toUpperCase()
+
+//estudianteMinus
+let estudianteMinus = (estudiante).toLowerCase()
+
+// number length
+let count = (estudiante.length)
+
+//variable firts letter
+
+let firts = name[0]
+
+//variable  letter
+let end = (last_name.split("")[last_name.length -1]);
+
+// espacios
+let student_split = estudiante.trim()
+
+
+
