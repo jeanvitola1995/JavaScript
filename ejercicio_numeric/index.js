cda4298f331eccb2f8280f35cfddb04278b1831b
jeanvitola1995 <jvitola@utb.edu.co>
2022-10-02 16:21:28 +0000

    openbootcamp

---
+++ ejercicio_numeric/index.js
@@ -0,0 +1,19 @@
+
+
+
+// Variable que contenga la altura en cm
+
+let jean_cm = 170
+
+//Variable que contenga la altura en m
+let jean_m = 1.70
+
+// variable que contenga el peso en kg
+let jean_peso = 78.3
+
+//variable que contenga la altura en metros redondeada
+
+let jean_m_redondeado = Math.round(jean_m)
+
+//Variable que contenga el peso redondeado hacia abajo
+let jean_peso_redondeado =Math.ceil(jean_peso)
